3d3c508660802db40ad61cbff6444e7eae140812
Lucas Simão <oliveira.lucas20@gmail.com>
2021-07-25 18:52:19 -0400

    Método addNumbers criado

--- app/src/main/java/com/example/calcnotapf/MainActivity.kt
+++ app/src/main/java/com/example/calcnotapf/MainActivity.kt
@@ -7,10 +7,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var nota = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnOne.setOnClickListener { addNumbers(btnOne.text.toString()) }
+        btnTwo.setOnClickListener { addNumbers(btnTwo.text.toString()) }
+        btnThree.setOnClickListener { addNumbers(btnThree.text.toString()) }
+        btnFour.setOnClickListener { addNumbers(btnFour.text.toString()) }
+        btnFive.setOnClickListener { addNumbers(btnFive.text.toString()) }
+        btnSix.setOnClickListener { addNumbers(btnSix.text.toString()) }
+        btnSeven.setOnClickListener { addNumbers(btnSeven.text.toString()) }
+        btnEigth.setOnClickListener { addNumbers(btnEigth.text.toString()) }
+        btnNine.setOnClickListener { addNumbers(btnNine.text.toString()) }
+        btnZero.setOnClickListener { addNumbers(btnZero.text.toString()) }
+
+    }
+
+    private fun addNumbers(n: String) {
+        nota = txtResult.text.toString() + n
+        txtResult.text = nota
     }
 
 }
