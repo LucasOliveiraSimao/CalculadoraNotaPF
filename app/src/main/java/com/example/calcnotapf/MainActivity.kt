a86b523d3d3d33a7d4f139fad454fb14cb4f7a0b
Lucas Simão <oliveira.lucas20@gmail.com>
2021-07-25 22:30:16 -0400

    Criação da função para calcular a nota necessária para a pf

--- app/src/main/java/com/example/calcnotapf/MainActivity.kt
+++ app/src/main/java/com/example/calcnotapf/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.calcnotapf
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,10 +25,61 @@ class MainActivity : AppCompatActivity() {
         btnEigth.setOnClickListener { addNumbers(btnEigth.text.toString()) }
         btnNine.setOnClickListener { addNumbers(btnNine.text.toString()) }
         btnZero.setOnClickListener { addNumbers(btnZero.text.toString()) }
+        btnPoint.setOnClickListener { addNumbers(btnPoint.text.toString()) }
 
         btnErase.setOnClickListener { erase() }
         btnAllErase.setOnClickListener { allErase() }
 
+        btnEquals.setOnClickListener { checks() }
+
+    }
+
+    private fun convert(): Double {
+        return (txtResult.text.toString().toDouble())
+    }
+
+    private fun calculate(): Double {
+        return abs(15 - (convert() * 2))
+
+    }
+
+    private fun checks() {
+        when {
+            convert() in 8.0..10.0 -> {
+                message(R.string.msg_parabens, R.string.msg_1, R.drawable.ic_aprovado_32)
+            }
+            convert() in 7.5..7.99 -> {
+                message(R.string.msg_alerta, R.string.msg_2, R.drawable.ic_alerta_32)
+            }
+            convert() in 2.5..7.49 -> {
+                message(
+                    R.string.msg_aviso,
+                    "Você precisa de " + calculate() + " ponto(s) para ser aprovado",
+                    R.drawable.ic_aviso_32
+                )
+            }
+            else -> {
+                message(R.string.msg_reprovado, R.string.msg_3, R.drawable.ic_reprovado_32)
+            }
+        }
+    }
+
+    private fun message(title: Int, msg: Int, icon: Int) {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle(title)
+        alert.setIcon(icon)
+        alert.setMessage(msg)
+        alert.setPositiveButton("Fechar", null)
+        alert.create().show()
+    }
+
+    private fun message(title: Int, msg: String, icon: Int) {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle(title)
+        alert.setIcon(icon)
+        alert.setMessage(msg)
+        alert.setPositiveButton("Fechar", null)
+        alert.create().show()
     }
 
     private fun addNumbers(n: String) {
@@ -42,7 +95,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun allErase(){
+    private fun allErase() {
         txtResult.text = ""
         txtResult.hint = "ME"
     }
